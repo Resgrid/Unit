f8e41283c9cbbdb2a19bc8b93d155566f3322224
Shawn Jackson <shawn@resgrid.com>
2025-02-26 15:11:59 -0800

    CU-868cu9311 clearing out for rn

--- src/app/features/calls/models/callAndPriorityData.ts
+++
@@ -1,6 +0,0 @@
-import { CallPriorityResultData, CallResultData } from '@resgrid/ngx-resgridlib';
-
-export class CallAndPriorityData {
-    public Call: CallResultData;
-    public CallPriority: CallPriorityResultData;
-}
